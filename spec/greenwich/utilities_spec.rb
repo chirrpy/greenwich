fd23d1e0b1c6bbfe793969d5b9a16ed9d5608811
Mark McEahern <mark@mceahern.com>
2012-08-14 22:47:03 -0500

    Add tests to utilities_spec to correspond to the actual methods in Greenwich::Utilities

--- spec/greenwich/utilities_spec.rb
+++ spec/greenwich/utilities_spec.rb
@@ -1,5 +1,118 @@
 require 'spec_helper'
 
 describe Greenwich::Utilities do
+  describe '.get_time_zone_field' do
+    subject { Greenwich::Utilities.get_time_zone_field(name, columns) }
 
+    let(:name) { 'foo_at' }
+
+    context 'when columns includes a field-specific time zone field' do
+      let(:columns) { %w(foo bar foo_at_time_zone) }
+
+      it 'is the field-specific time zone field' do
+        subject.should eql 'foo_at_time_zone'
+      end
+    end
+
+    context 'when columns does not include a field-specific time zone field' do
+      context 'but does include the general time zone field' do
+        let(:columns) { %w(foo bar time_zone) }
+
+        it 'is the general time zone field' do
+          subject.should eql 'time_zone'
+        end
+      end
+
+      context 'and does not include the general time zone field' do
+        let(:columns) { %w(foo bar) }
+
+        it 'is nil' do
+          subject.should be_nil
+        end
+      end
+
+    end
+  end
+
+  describe '.get_time_field' do
+    subject { Greenwich::Utilities.get_time_field(name, columns) }
+
+    let(:name) { 'foo' }
+
+    context 'when columns includes all of the target columns' do
+      let(:columns) { %w(foo bar foo_at foo_datetime foo_time) }
+
+      it 'is the first available one' do
+        subject.should eql 'foo_at'
+      end
+    end
+
+    context 'when columns includes only some of the target columns' do
+      let(:columns) { %w(foo bar foo_datetime foo_time) }
+
+      it 'is the first available one' do
+        subject.should eql 'foo_datetime'
+      end
+    end
+
+    context 'when columns includes only one of the target columns' do
+      let(:columns) { %w(foo bar foo_time) }
+
+      it 'is the first available one' do
+        subject.should eql 'foo_time'
+      end
+    end
+
+    context 'when columns does not include any of the target columns' do
+      let(:columns) { %w(foo bar) }
+
+      it 'is nil' do
+        subject.should be_nil
+      end
+    end
+  end
+
+  describe '.get_time_zone_from' do
+    subject { Greenwich::Utilities.get_time_zone_from(value) }
+
+    context 'when the value is nil' do
+      let(:value) { nil }
+
+      it 'is nil' do
+        subject.should be_nil
+      end
+    end
+
+    context 'when the value is blank' do
+      let(:value) { '' }
+
+      it 'is nil' do
+        subject.should be_nil
+      end
+    end
+
+    context 'when the value is not the name of a valid time zone' do
+      let(:value) { 'foo' }
+
+      it 'is nil' do
+        subject.should be_nil
+      end
+    end
+
+    context 'when the value is the name of a valid time zone' do
+      let(:value) { 'Alaska' }
+
+      it 'is the corresponding TimeZone' do
+        subject.should eql ActiveSupport::TimeZone.new('Alaska')
+      end
+    end
+
+    context 'when the value is a time zone' do
+      let(:value) { ActiveSupport::TimeZone.new('Alaska') }
+
+      it 'is the time zone' do
+        subject.should eql value
+      end
+    end
+  end
 end
